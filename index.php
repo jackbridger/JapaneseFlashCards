652ce59fbc9489f57788aa78902fb87b69c68b46
Jack Bridger <jacksbridger@gmail.com>
2019-06-12 15:37:24 +0100

    setup heroku, adding php file to deploy static site

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+<!-- Creating a line PHP file to run a static site on heroku -->
+<!-- Reference: https://blog.teamtreehouse.com/deploy-static-site-heroku -->
