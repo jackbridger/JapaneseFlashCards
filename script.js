a543a55dc00fd604b3b914be008174375b8d14b1
Jack Bridger <jacksbridger@gmail.com>
2019-06-20 16:10:06 +0100

    counter working

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const btnOkCounter = document.getElementById('card-ok');
 const btnGoodCounter = document.getElementById('card-good');
 const btnExcellentCounter = document.getElementById('card-excellent');
 
+const timerSpace = document.getElementById('timer-status');
+
+let time = new Date();
+const timeStart = time.getTime();
+
 function updateCounter() {
     btnUntestedCounter.innerHTML = all_arrs[0].length
     btnBadCounter.innerHTML = all_arrs[1].length;
@@ -11,6 +16,7 @@ function updateCounter() {
     btnGoodCounter.innerHTML = all_arrs[3].length;
     btnExcellentCounter.innerHTML = all_arrs[4].length;
 }
+
 function setUp() {
     updateCounter();
     document.getElementById('english-text').innerText = list_phrases[all_arrs[current_pile][0]].englishText;
@@ -54,24 +60,24 @@ for (let i = 1; i <= list_length; i++) {
     all_arrs[0].push(i);
 }
 
-    card_question = document.getElementById('flash-container-before');
-    card_answer = document.getElementById('flash-container-after');
-    card_question.style.visibility = 'visible';
-    card_answer.style.visibility = 'hidden';
+card_question = document.getElementById('flash-container-before');
+card_answer = document.getElementById('flash-container-after');
+card_question.style.visibility = 'visible';
+card_answer.style.visibility = 'hidden';
 
-    flipCard = function() {
-        if (card_question.style.visibility === 'visible') {
-            card_question.style.visibility = 'hidden';}
-        else {
-            card_question.style.visibility = 'visible';} 
+flipCard = function() {
+    if (card_question.style.visibility === 'visible') {
+        card_question.style.visibility = 'hidden';}
+    else {
+        card_question.style.visibility = 'visible';} 
 
-        if (card_answer.style.visibility === 'visible') {
-            card_answer.style.visibility = 'hidden';}
-        else {
-            card_answer.style.visibility = 'visible';} 
-    };
-    
-    card_question.addEventListener('click', flipCard);
+    if (card_answer.style.visibility === 'visible') {
+        card_answer.style.visibility = 'hidden';}
+    else {
+        card_answer.style.visibility = 'visible';} 
+};
+
+card_question.addEventListener('click', flipCard);
 
 
 function storeInformation(button) {
@@ -106,5 +112,19 @@ function storeInformation(button) {
 
     console.log(all_arrs);
 }
+
+var timer = setInterval(function() {
+    let currentTime = new Date();
+    let timeElapsed = currentTime.getTime() - timeStart;
+    timeElapsed = Math.floor(timeElapsed / 1000);
+    let minutes = Math.floor(timeElapsed / 60);
+    let seconds = timeElapsed - (minutes * 60)
+    if (minutes > 0)
+        timerSpace.innerHTML = minutes + 'm ' + seconds + 's'
+    else 
+        timerSpace.innerHTML = seconds + 's';
+}, 1000);
+
+
 
 document.onload = setUp();
