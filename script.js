65119a75b852b0f056efb35073c68acde550c994
Jack Bridger <jacksbridger@gmail.com>
2019-06-20 13:01:59 +0100

    basic functionality is working

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btnOkCounter = document.getElementById('card-ok');
 const btnGoodCounter = document.getElementById('card-good');
 const btnExcellentCounter = document.getElementById('card-excellent');
 
+let current_pile = 0;
+
 const list_phrases = 
 {
     1: {
@@ -30,9 +32,6 @@ const list_phrases =
 
 const list_length = _.size(list_phrases);
 
-
-let card_obj = function(){
-    
     
     card_question = document.getElementById('flash-container-before');
     card_answer = document.getElementById('flash-container-after');
@@ -53,61 +52,39 @@ let card_obj = function(){
     
     card_question.addEventListener('click', flipCard);
 
-}();
-
-
-let phrases = function() {
 
 
 
-    let untested_arr = [1,2,3,4,5];
-    let bad_arr = [];
-    var current = untested_arr.shift();
-    console.log('take off the top of the untested queue: ' + list_phrases[current].japaneseText);
-    bad_arr.push(current);
-    console.log('end of the bad queue: ' + list_phrases[bad_arr[0]].japaneseText);
-    
-
-
-
-    japanese_text = document.getElementById('japanese-text');
-    english_text = document.getElementById('english-text');
-
-}();
-
-let untested_arr = [1,2,3,5,6];
-let bad_arr = [];
-let ok_arr = [];
-let good_arr = [];
-let excellent_arr = [];
+let all_arrs = [[1,2,3,4],[],[],[],[]];
 
 function storeInformation(button) {
-    var current = '';
+    current = all_arrs[current_pile].shift();
+
     if (button === 'bad') {
-        current = untested_arr.shift();
-        console.log('take off the top of the untested queue: ' + list_phrases[current].japaneseText);
-        bad_arr.push(current);
-        console.log('end of the bad queue: ' + list_phrases[bad_arr[0]].japaneseText);
+        all_arrs[1].push(current);
     }
     else if (button === 'ok') {
-        current = untested_arr.shift();
-        console.log('take off the top of the untested queue: ' + list_phrases[current].japaneseText);
-        ok_arr.push(current);
-        console.log('end of the bad queue: ' + list_phrases[ok_arr[0]].japaneseText);        
+        all_arrs[2].push(current);    
     }
     else if (button === 'good') {
-        current = untested_arr.shift();
-        console.log('take off the top of the untested queue: ' + list_phrases[current].japaneseText);
-        good_arr.push(current);
-        console.log('end of the bad queue: ' + list_phrases[good_arr[0]].japaneseText);        
+        all_arrs[3].push(current);   
     }
     else if (button === 'excellent') {
-        current = untested_arr.shift();
-        excellent_arr.push(current);
+        all_arrs[4].push(current);
     }
-    if (excellent_arr.length === list_length)
+    if (all_arrs[4].length === list_length)
     {
         alert('completed everything')
     }
 
+    for (var i = 4; i >= 0; i--) {
+        if (all_arrs[i].length > 0){
+            current_pile = i;
+        }
+    }
+        flipCard();
+        document.getElementById('english-text').innerText = list_phrases[all_arrs[current_pile][0]].englishText;
+        document.getElementById('japanese-text').innerText = list_phrases[all_arrs[current_pile][0]].japaneseText;
+
+    console.log(all_arrs);
 }
